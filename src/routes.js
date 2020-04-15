88ee8994cc1ae26ede10fc9b09f857e56470e8cd
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-04-15 11:56:50 -0300

    cria rotas de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 
-
+const UsuarioController = require('./controllers/UsuarioController.js')
 
 const routes = express.Router()
 
+routes.get('/usuario/:id', UsuarioController.find)
+routes.get('/usuario', UsuarioController.findAll)
+routes.put('/usuario', UsuarioController.update)
+routes.delete('/usuario/:id', UsuarioController.find)
 
 
 module.exports = routes
